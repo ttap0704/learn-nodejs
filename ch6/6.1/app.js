f0675d020a0a62c0ec5f367e816b8e2acf70424a
LeeDaeGyu <ttap0704@naver.com>
2021-07-25 22:13:53 +0900

    2021. 07. 25 express 학습

--- ch6/6.1/app.js
+++ ch6/6.1/app.js
@@ -1,22 +1,65 @@
 const { ESRCH } = require('constants');
 const { resolveSoa } = require('dns');
+
+const fs = require('fs')
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const multer = require('multer');
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(morgan('dev'))
+app.use(cookieParser('daegyupassword'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'deegyupassword',
+  cookie: {
+    httpOnly: true,
+  },
+}));
+app.use('/', (req, res, next) => {
+  if (req.session, id) {
+    express.static(paht.join(__dirname, 'public-3030'))(req, res, next)
+  } else {
+    next()
+  }
+}); // 미들웨어 확장법 
+
+app.use(multer().array());
+
 app.use((req, res, next) => {
-  console.log('모든 요청!');
-  next();
+  req.session.data = '비번'; // 계속 남아있는 데이터
+  req.data = '비번'; // 일회성 데이터
 })
 
+
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-  next('route')
+  // req.cookies // { mycookie: 'test }
+  // req.signedCookies;
+
+  // 'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+  // res.cookie('name', encodeURIComponent(name),{
+  //   expires: new Date(),
+  //   httpOnly: true,
+  //   path: '/'
+  // })
+  // res.clearCookie('name', encodeURIComponent(name),{
+  //   httpOnly: true,
+  //   path: '/'
+  // })
   // res.json({ hello: 'daegyu lee'});
-}, (req, res) => {
-  console.log('실행되나요?');
+
+  req.session.data; // 비번
+  req.data; // 비번
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.get('/', (req, res, next) => {
@@ -35,8 +78,36 @@ app.get('/about', (req, res) => {
   res.send('hello about');
 })
 
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.log('no upload folder');
+  fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads');
+    },
+    filename(req, file, done) {
+      const ext = file.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }
+})
+
+app.get('/uploads', (req, res) => {
+  res.sendFile(path.join(__dirname, 'multipart.html'))
+})
+app.get('/uploads', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  res.send('ok')
+})
+
 app.get((req, res, next) => {
- res.status(200).send('404 지롱');
+  res.status(404).send('404 지롱');
 })
 
 app.use((err, req, res, next) => {
@@ -44,6 +115,6 @@ app.use((err, req, res, next) => {
   res.status(200).send('에러다 에러!')
 })
 
-app.listen(app.get('port'), ()=> {
+app.listen(app.get('port'), () => {
   console.log('익스프레스 서버 실행');
 });
